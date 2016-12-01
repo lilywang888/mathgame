caa3a8818e12efbc0e169b550bf9892dca209124
Li Wang <lilywang888@gmail.com>
2016-11-30 21:20:48 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Math Game</title>
+    <link href="./Styles/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+             <div class="col-sm-10 col-sm-offset-1"><h1>Math Game</h1></div>
+             <div class="col-sm-1"></div>
+        </div>
+        <div class="row"></div>
+    </div>
+</body>
+</html>
